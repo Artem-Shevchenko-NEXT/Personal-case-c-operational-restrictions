45d52014e4539d7307bc373b9953e0037f62dfa1
SalemZad97 <Salemzad@gmail.com>
2026-09-17 19:55:34 +0200

    Install and configure ESLint for frontend

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['**/*.{js,ts,tsx,jsx}'],
+  extends: [js.configs.recommended, tseslint.configs.recommended],
+});
